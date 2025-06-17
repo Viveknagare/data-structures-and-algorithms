469c49fd80cded17f98b7f69a900943d3d54b4da
Viveknagare <vivekbnagare31@gmail.com>
2025-06-17 15:16:50 +0530

    added tabulation with space optimization

--- Dynamic_programming/src/Subset_Sum_Equal_K.java
+++ Dynamic_programming/src/Subset_Sum_Equal_K.java
@@ -3,23 +3,27 @@
 public class Subset_Sum_Equal_K {
     public static boolean subsetSumToK(int n, int k, int arr[]){
         // Write your code here.
-        boolean[][]dp = new boolean[n][k + 1];
-        for(int i = 0; i < n; i++){
-            dp[i][0] = true;
-        }
-        if(arr[0] <= k) dp[0][arr[0]] = true;
+        boolean[] prev = new boolean[k + 1];
 
+        prev[0] = true;
+
+        if(arr[0] <= k) prev[arr[0]] = true;
+ 
         for(int ind = 1; ind < n; ind++){
+            boolean[] curr = new boolean[k + 1];
+            curr[0] = true;
             for(int target = 1; target <= k; target++){
-               boolean nottake = dp[ind - 1][target];
+
+               boolean nottake = prev[target];
                boolean take = false;
                if(target >= arr[ind]){
-                   take = dp[ind - 1][target - arr[ind]];
+                   take = prev[target - arr[ind]];
                }
-               dp[ind][target] = take || nottake;
+               curr[target] = take || nottake;
             }
+            prev = curr;
         }
-        return dp[n - 1][k];
+        return prev[k];
     }
     public static void main(String[] args) {
         int[]arr = {1,2,3,4};
